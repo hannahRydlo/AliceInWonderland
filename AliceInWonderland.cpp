8f70d0f640aec660f82ade7c3e0dfa201230d083
hannahRydlo <119124176+hannahRydlo@users.noreply.github.com>
2024-02-29 14:43:05 -0500

    Add files via upload

---
+++ AliceInWonderland.cpp
@@ -0,0 +1,36 @@
+/*
+Title: Assignment 4
+Description: Alice in wonderland game
+Date: March 1, 2024
+Author: Hannah Rydlo
+Version: 1.0
+Copyright: 2024 Hannah Rydlo
+*/
+
+/*
+DOCUMENTATION
+Program Purpose: 
+    
+    Compile (compiled on bash): g++ AliceInWonderland -o AliceInWonderland 
+    Execution (compiled on bash): ./AliceInWonderland 
+Classes: none
+Variables:
+
+*/
+
+/*
+TEST PLAN
+the test plan is simply; does the output match the required format?
+
+Discussion:
+
+*/
+
+#include <iostream>
+class Game {
+
+};
+
+int main(void){
+
+}
